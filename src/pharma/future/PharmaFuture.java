2c9a12adb52e4e5dd5673f7a770d77dace596d13
salvospe4 <salvatorespezia98@gmail.com>
2022-05-30 18:57:50 +0200

    new changes

--- src/pharma/future/PharmaFuture.java
+++ src/pharma/future/PharmaFuture.java
@@ -15,6 +15,7 @@ public class PharmaFuture {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("New change!!!");
     }
     
 }
