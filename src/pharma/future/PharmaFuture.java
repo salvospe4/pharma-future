cba17760013cdcbe1ade3ada511101f11d5881f3
salvospe4 <salvatorespezia98@gmail.com>
2022-05-30 18:50:54 +0200

    my initial commit

--- src/pharma/future/PharmaFuture.java
+++ src/pharma/future/PharmaFuture.java
@@ -14,7 +14,7 @@ public class PharmaFuture {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
     }
     
 }
