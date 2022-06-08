d8ff2c724da01cc9632ed37af43ef0cf3861d7f3
salvospe4 <salvatorespezia98@gmail.com>
2022-06-08 21:23:20 +0200

    new changes

--- src/pharma/future/SchermataAccedi.java
+++ src/pharma/future/SchermataAccedi.java
@@ -43,6 +43,7 @@ public class SchermataAccedi extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
